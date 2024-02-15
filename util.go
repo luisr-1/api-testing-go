e8abbf4de30ea2765632d5797aea8fb8ceadf2e4
Luis Rodrigues <luisr.silva@outlook.com>
2024-02-15 13:58:37 -0300

    feature: criada função para gerar ID, criada função para salvar dados para os testes

--- util.go
+++ util.go
@@ -1,6 +1,11 @@
 package main
 
 import (
+	"math/rand"
+	"encoding/json"
+	"fmt"
+	"net/http"
+
 	"github.com/brianvoe/gofakeit/v6"
 )
 
@@ -20,4 +25,19 @@ func (u *User) CreateUser(adm bool) {
 	} else {
 		u.Administrador = "false"
 	}
+}
+
+func SaveData(resp *http.Response, data *map[string]interface{}) {
+	if err := json.NewDecoder(resp.Body).Decode(&data); err != nil {
+		fmt.Errorf("Aconteceu o erro %e", err)
+	}
+}
+
+func GenerateID(length int) string {
+	var possibleCharacters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789")
+    b := make([]rune, length)
+    for i := 0; i < length; i++ {
+        b[i] = possibleCharacters[rand.Intn(len(possibleCharacters))]
+    }
+    return string(b)
 }
